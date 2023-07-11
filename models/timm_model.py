9d7d989e118633c31724d23b31d057b7a65f1ce6
Xuan Bac Nguyen <xnguyen@uark.edu>
2023-07-11 04:13:35 -0500

    convnext

--- models/timm_model.py
+++ models/timm_model.py
@@ -3,6 +3,42 @@ import timm
 import torch
 import os
 from torchvision import models as torchvision_models
+from .convnext import convnext_xlarge, convnext_large
+
+
+convnext_coco_checkpoint = {
+    "convnext_xlarge_coco": "https://dl.fbaipublicfiles.com/convnext/coco/cascade_mask_rcnn_convnext_xlarge_22k_3x.pth",
+    "convnext_large_coco": "https://dl.fbaipublicfiles.com/convnext/coco/cascade_mask_rcnn_convnext_large_22k_3x.pth",
+}
+
+
+def get_convnext_coco(model_name):
+    if model_name == "convnext_xlarge_coco":
+        model = convnext_xlarge()
+    elif model_name == "convnext_large_coco":
+        model = convnext_large()
+
+    model_state_dict = model.state_dict()
+    url = convnext_coco_checkpoint[model_name]
+    checkpoint = torch.hub.load_state_dict_from_url(url=url, map_location="cpu")[
+        "state_dict"
+    ]
+    backbone_dict = {}
+    for k, v in checkpoint.items():
+        if "backbone" in k:
+            k_ = k.replace("backbone.", "")
+            if k_ in model_state_dict:
+                backbone_dict[k_] = v
+            else:
+                print(f"{k_} is not used.")
+
+    for k, v in model_state_dict.items():
+        if not k in backbone_dict:
+            print(f"{k} is initialized by default.")
+            backbone_dict[k] = model_state_dict[k]
+
+    model.load_state_dict(backbone_dict)
+    return model
 
 
 class AlgonautsTimm(nn.Module):
@@ -17,15 +53,18 @@ class AlgonautsTimm(nn.Module):
             self.backbone = torchvision_models.__dict__[args.model_name](
                 weights="DEFAULT"
             )
-            if hasattr(self.backbone, "fc"):
-                embed_dim = self.backbone.fc.weight.shape[1]
-                self.backbone.fc = nn.Identity()
-            elif hasattr(self.backbone, "classifier"):
-                embed_dim = self.backbone.classifier[-1].weight.shape[1]
-                self.backbone.classifier = nn.Identity()
-            elif hasattr(self.backbone, "head"):
-                embed_dim = self.backbone.head.weight.shape[1]
-                self.backbone.head = nn.Identity()
+        else:
+            self.backbone = get_convnext_coco(args.model_name)
+
+        if hasattr(self.backbone, "fc"):
+            embed_dim = self.backbone.fc.weight.shape[1]
+            self.backbone.fc = nn.Identity()
+        elif hasattr(self.backbone, "classifier"):
+            embed_dim = self.backbone.classifier[-1].weight.shape[1]
+            self.backbone.classifier = nn.Identity()
+        elif hasattr(self.backbone, "head"):
+            embed_dim = self.backbone.head.weight.shape[1]
+            self.backbone.head = nn.Identity()
 
         if os.path.isfile(args.pretrained):
             checkpoint = torch.load(args.pretrained)["model"]
